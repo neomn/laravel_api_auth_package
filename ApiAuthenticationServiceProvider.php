2920e357a1a3e9b0e5c1385a8b9b84cd70348aa4
neomn110 <neomn110@gmail.com>
2022-10-24 13:29:38 +0330

    feat: create a service provider

---
+++ ApiAuthenticationServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers\package\laravel_auth_api\src;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiAuthenticationServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
