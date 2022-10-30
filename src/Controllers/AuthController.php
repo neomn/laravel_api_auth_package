bbea86ec52d4f5a351d72255cc1c257b753b44ef
neomn110 <neomn110@gmail.com>
2022-10-30 14:05:15 +0330

    feat: add auth controller function bodies

--- src/Controllers/AuthController.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace neomn\laraauthapi\Controllers;
-
-class AuthController extends Controller
-{
-
-}
---
+++ src/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace neomn\laraauthapi\Http\Controllers;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+
+    public function verifyUser()
+    {
+
+    }
+
+    public function updatePhoneNumber()
+    {
+
+    }
+
+    public function updateUserInfo()
+    {
+
+    }
+
+    public function forgetPassword()
+    {
+
+    }
+}
