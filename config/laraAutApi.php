6c33e415f8d3650b5d41eeff5824bd6c5a2a70da
neomn110 <neomn110@gmail.com>
2022-11-01 09:26:12 +0330

    feat: add auth related configs to laraAuthApi Config file

--- config/laraAutApi.php
+++ config/laraAutApi.php
@@ -1,4 +1,15 @@
 <?php
-return [
 
+$authMethods = [
+    'username'=>'username',
+    'email'=>'email',
+    'phone_number'=>'phone_number',
+    'social_media'=>'social_media',
+];
+
+return [
+    'auth'=>[
+        'authDefaultMethod'=> $authMethods['username'],
+        '2factorAuth'=>false,
+    ],
 ];
