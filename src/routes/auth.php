73737004d0808d3c83ff9244f413f1da6b591951
neomn110 <neomn110@gmail.com>
2022-11-07 21:42:59 +0330

    feat: add forget password endpoint

--- src/routes/auth.php
+++ src/routes/auth.php
@@ -9,6 +9,7 @@ Route::middleware('auth:api')->prefix('auth')->group(function (){
     Route::post('login', [AuthController::class,'login'])->withoutMiddleware('auth:api');
     Route::post('register', [AuthController::class,'register'])->withoutMiddleware('auth:api');
     Route::post('verify-user', [AuthController::class,'verifyUser'])->withoutMiddleware('auth:api');
+    Route::post('forget-password', [AuthController::class,'forgetPassword'])->withoutMiddleware('auth:api');
     Route::post('update-phone-number', [AuthController::class,'updatePhoneNumber']);
-    Route::post('update-user-info', [AuthController::class,'updatePhoneNumber']);
+    Route::post('update-user-info', [AuthController::class,'updateUserInfo']);
 });
