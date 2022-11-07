8332ea0a137ee8acccdbfcebd0891ba215df6b78
neomn110 <neomn110@gmail.com>
2022-11-07 17:28:21 +0330

    feat: add register endpoint

--- src/routes/auth.php
+++ src/routes/auth.php
@@ -6,5 +6,6 @@ use Illuminate\Support\Facades\Route;
 use neomn\laraauthapi\Http\Controllers\AuthController;
 
 Route::middleware('auth:api')->prefix('auth')->group(function (){
-    Route::get('login', AuthController::class,'login')->withoutMiddleware('auth:api');
+    Route::get('login', [AuthController::class,'login'])->withoutMiddleware('auth:api');
+    Route::get('register', [AuthController::class,'register'])->withoutMiddleware('auth:api');
 });
