f87fd4d467ad858af955d1327c31344c4d1939b1
Neo <neomn110@gmail.com>
2022-11-08 17:05:26 +0330

    feat: send a test register response

--- tests/Feature/Auth/AuthTest.php
+++ tests/Feature/Auth/AuthTest.php
@@ -8,8 +8,8 @@ class AuthTest extends TestCase
 {
     public function test_auth_routes_exists()
     {
-        $response = $this->post('/auth/login');
-        $response = $this->post('/auth/login');
+        $loginResponse = $this->post('api/auth/login');
+        $registerResponse = $this->post('api/auth/register');
 //        $response->assertJson('auth/login',);
 
     }
