a8764d86882a4fbdee0f16b18b3db361cf541360
Neo <neomn110@gmail.com>
2022-11-08 17:01:53 +0330

    refactor: change route file structure

--- src/routes/auth.php
+++ src/routes/auth.php
@@ -4,13 +4,15 @@ namespace neomn\laraauthapi\routes;
 
 use Illuminate\Support\Facades\Route;
 use neomn\laraauthapi\Http\Controllers\AuthController;
+Route::prefix('api')->middleware('auth:api')->group(function (){
 
-Route::middleware('auth:api')->prefix('auth')->group(function (){
-    Route::post('login', [AuthController::class,'login'])->withoutMiddleware('auth:api');
-    Route::post('register', [AuthController::class,'register'])->withoutMiddleware('auth:api');
-    Route::post('verify-user', [AuthController::class,'verifyUser'])->withoutMiddleware('auth:api');
-    Route::post('forget-password', [AuthController::class,'forgetPassword'])->withoutMiddleware('auth:api');
-    Route::post('update-phone-number', [AuthController::class,'updatePhoneNumber']);
-    Route::post('update-user-info', [AuthController::class,'updateUserInfo']);
-    Route::post('logout', [AuthController::class,'logout']);
+    Route::prefix('auth')->group(function (){
+        Route::post('login', [AuthController::class,'login'])->withoutMiddleware('auth:api');
+        Route::post('register', [AuthController::class,'register'])->withoutMiddleware('auth:api');
+        Route::post('verify-user', [AuthController::class,'verifyUser'])->withoutMiddleware('auth:api');
+        Route::post('forget-password', [AuthController::class,'forgetPassword'])->withoutMiddleware('auth:api');
+        Route::post('update-phone-number', [AuthController::class,'updatePhoneNumber']);
+        Route::post('update-user-info', [AuthController::class,'updateUserInfo']);
+        Route::post('logout', [AuthController::class,'logout']);
+    });
 });
--- tests/Feature/Auth/AuthTest.php
+++ tests/Feature/Auth/AuthTest.php
@@ -9,7 +9,8 @@ class AuthTest extends TestCase
     public function test_auth_routes_exists()
     {
         $response = $this->post('/auth/login');
-//        $response->assertJsonStructure();
-        $response->assertStatus(200);
+        $response = $this->post('/auth/login');
+//        $response->assertJson('auth/login',);
+
     }
 }
