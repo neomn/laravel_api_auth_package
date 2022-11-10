10fb3129f08db437c488ed91b6d51bb6349a7f30
neomn110 <neomn110@gmail.com>
2022-11-10 07:20:19 +0330

    feat: add a test response to register

--- src/Http/Controllers/AuthController.php
+++ src/Http/Controllers/AuthController.php
@@ -11,7 +11,7 @@ class AuthController extends Controller
 
     public function register()
     {
-
+        return response()->json('register');
     }
 
     public function verifyUser()
--- tests/Feature/Auth/AuthTest.php
+++ tests/Feature/Auth/AuthTest.php
@@ -8,13 +8,21 @@ class AuthTest extends TestCase
 {
     public function test_auth_routes_exists()
     {
-        $loginResponse = $this->post('api/auth/login');
-        $registerResponse = $this->post('api/auth/register');
-        $verifyUserResponse = $this->post('api/auth/verify-user');
-        $forgetPasswordResponse = $this->post('api/auth/forge-password');
-        $updatePhoneNumberResponse = $this->post('api/auth/update-phone-number');
-        $updateUserInfoResponse = $this->post('api/auth/update-user-info');
-        $logoutResponse = $this->post('api/auth/logout');
+        $endpoints = [
+            $loginResponse = $this->post('api/auth/login'),
+            $registerResponse = $this->post('api/auth/register'),
+            $verifyUserResponse = $this->post('api/auth/verify-user'),
+            $forgetPasswordResponse = $this->post('api/auth/forge-password'),
+            $updatePhoneNumberResponse = $this->post('api/auth/update-phone-number'),
+            $updateUserInfoResponse = $this->post('api/auth/update-user-info'),
+            $logoutResponse = $this->post('api/auth/logout'),
+        ];
+
+//        dd($loginResponse->status());
+
+        foreach($endpoints as $key => $endpoint){
+            $this->assertTrue($endpoint->status() != 404);
+        }
 
     }
 }
