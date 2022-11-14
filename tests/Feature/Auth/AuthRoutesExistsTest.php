28e9298e0d51f5297b104e29f6e2c1993a4e0fdf
Neo <neomn110@gmail.com>
2022-11-14 15:00:44 +0330

    refactor: rename authtest to authRoutesExistsTest

---
+++ tests/Feature/Auth/AuthRoutesExistsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace neomn\laravel_auth_api\tests\Feature\Auth;
+
+use Illuminate\Support\Facades\Log;
+use neomn\laravel_auth_api\tests\TestCase;
+
+class AuthRoutesExistsTest extends TestCase
+{
+    public function test_auth_routes_exists()
+    {
+        $endpoints_not_requiring_auth = [
+            'login' => $loginResponse = $this->post('api/auth/login'),
+            'register' => $registerResponse = $this->post('api/auth/register'),
+            'verify-user' => $verifyUserResponse = $this->post('api/auth/verify-user'),
+            'forge-password' => $forgetPasswordResponse = $this->post('api/auth/forge-password'),
+        ];
+        foreach($endpoints_not_requiring_auth as $key => $endpoint){
+           echo ('checking >> ' . $key);
+            $this->assertTrue($endpoint->status() != 404);
+        }
+
+//        $endpoints_requiring_auth = [
+//            $updatePhoneNumberResponse = $this->post('api/auth/update-phone-number'),
+//            $updateUserInfoResponse = $this->post('api/auth/update-user-info'),
+//            $logoutResponse = $this->post('api/auth/logout'),
+//        ];
+//        foreach($endpoints_requiring_auth as $key => $endpoint){
+//            $this->assertTrue($endpoint->status() == 401);
+//        }
+
+    }
+}
