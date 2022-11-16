27af5ac85442141051f775217d826f2a33569a9f
Neo <neomn110@gmail.com>
2022-11-16 15:35:08 +0330

    refactor: successful test for route update user info

--- tests/Feature/Auth/AuthRoutesExistsTest.php
+++ tests/Feature/Auth/AuthRoutesExistsTest.php
@@ -44,7 +44,7 @@ class AuthRoutesExistsTest extends TestCase
 
     public function test_update_user_info_route_exists()
     {
-        $response = $this->post('api/auth/update-user-info');
+        $response = $this->json('post' ,'api/auth/update-user-info');
         $response->assertStatus(401);
     }
 
