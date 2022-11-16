9d961ae423ab25fc19032e53ea6b034af6497687
Neo <neomn110@gmail.com>
2022-11-16 15:59:55 +0330

    feat: add template for login test

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace neomn\laravel_auth_api\tests\Feature\Auth;
+
+use neomn\laravel_auth_api\tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    public function test_logged_in_successfully()
+    {
+        $this->assertTrue(true);
+    }
+}
