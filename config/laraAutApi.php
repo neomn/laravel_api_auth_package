1ce73f6610ee42595c5ae4d8847774d0fb86cf54
neomn110 <neomn110@gmail.com>
2022-11-18 19:26:01 +0330

    refactor: remove unnecessary code

--- config/laraAutApi.php
+++ config/laraAutApi.php
@@ -1,29 +1,25 @@
 <?php
 
-// set registration method to true/false to set them enable/disable
-$registrationMethods = [
-    'phone_number'=>true,
-    'email'=>true,
-    'username'=>true,
-    'social_media'=>true,
-];
-
-$loginMethods = [
-    'username'=>true,
-    'email'=>true,
-    'phone_number'=>true,
-    'social_media'=>true,
-];
-
-$twoFactorAuthMethod = [
-    'phone_number'=>true,
-    'email'=>true,
-    'google_authenticator'=>true,
-];
-
 return [
-    'auth'=>[
-        'defaultAuthMethod'=> $authMethods['username'],
-        'defaultTwoFactorAuthMethod'=>'disable',
+
+// set registration method to true/false to set them enable/disable
+    $registrationMethods = [
+        'phone_number' => true,
+        'email' => true,
+        'username' => true,
+        'social_media' => true,
+    ],
+
+    $loginMethods = [
+        'username' => true,
+        'email' => true,
+        'phone_number' => true,
+        'social_media' => true,
+    ],
+
+    $twoFactorAuthMethod = [
+        'phone_number' => true,
+        'email' => true,
+        'google_authenticator' => true,
     ],
 ];
