f07ee6d76227ae49aa0ab3e3c1e77996cca766a1
neomn110 <neomn110@gmail.com>
2022-11-19 13:23:32 +0330

    feat: assign register middleware to register route

--- src/routes/auth.php
+++ src/routes/auth.php
@@ -4,12 +4,13 @@ namespace neomn\laravel_auth_api\routes;
 
 use Illuminate\Support\Facades\Route;
 use neomn\laravel_auth_api\Http\Controllers\AuthController;
+use neomn\laravel_auth_api\Http\Middleware\Register;
 
 Route::prefix('api')->middleware('auth')->group(function (){
 
     Route::prefix('auth')->group(function (){
         Route::post('login', [AuthController::class,'login'])->withoutMiddleware('auth');
-        Route::post('register', [AuthController::class,'register'])->withoutMiddleware('auth');
+        Route::post('register', [AuthController::class,'register'])->withoutMiddleware('auth')->middleware(Register::class);
         Route::post('verify-user', [AuthController::class,'verifyUser'])->withoutMiddleware('auth');
         Route::post('update-user-info', [AuthController::class,'updateUserInfo']);
         Route::post('update-phone-number', [AuthController::class,'updatePhoneNumber']);
