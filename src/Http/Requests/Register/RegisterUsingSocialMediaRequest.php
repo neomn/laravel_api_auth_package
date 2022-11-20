6ae7f533d9e49a8613566347acde6d27a1b7f1d0
neomn110 <neomn110@gmail.com>
2022-11-20 18:09:50 +0330

    feat: create RegisterUsingSocialMedia request

---
+++ src/Http/Requests/Register/RegisterUsingSocialMediaRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace neomn\laravel_auth_api\Http\Requests\Register;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUsingSocialMediaRequest extends FormRequest
+{
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
