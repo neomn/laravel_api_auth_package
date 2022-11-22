73ec1cfdea562863d91a5163df5248830ac9578a
Neo <neomn110@gmail.com>
2022-11-22 09:16:18 +0330

    feat: add register route group

--- src/routes/auth.php
+++ src/routes/auth.php
@@ -4,11 +4,20 @@ namespace neomn\laravel_auth_api\routes;
 
 use Illuminate\Support\Facades\Route;
 use neomn\laravel_auth_api\Http\Controllers\AuthController;
+use neomn\laravel_auth_api\Http\Controllers\Register\RegisterController;
 use neomn\laravel_auth_api\Http\Middleware\Register;
 
 Route::prefix('api')->middleware('auth')->group(function (){
 
     Route::prefix('auth')->group(function (){
+
+        Route::prefix('register')->middleware('Register')->withoutMiddleware('auth')->group(function (){
+            Route::post('username',[RegisterController::class,'registerUsingUserName']);
+            Route::post('email',[RegisterController::class,'registerUsingEmail']);
+            Route::post('phone-number',[RegisterController::class,'registerUsingPhoneNumber']);
+            Route::post('social-media',[RegisterController::class,'registerUsingSocialMedia']);
+        });
+
         Route::post('login', [AuthController::class,'login'])->withoutMiddleware('auth');
         Route::post('register', [AuthController::class,'register'])->withoutMiddleware('auth')->middleware(Register::class);
         Route::post('verify-user', [AuthController::class,'verifyUser'])->withoutMiddleware('auth');
