bb1c49540c08cdbe24515d2b910b27f79e5b6ae1
Neo <neomn110@gmail.com>
2022-11-22 08:57:42 +0330

    feat: create a register controller

---
+++ src/Http/Controllers/RegisterController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace neomn\laravel_auth_api\Http\Controllers\Register;
+
+use neomn\laravel_auth_api\Http\Controllers\Controller;
+use neomn\laravel_auth_api\Http\Requests\Register\RegisterUsingUserNameRequest;
+
+class RegisterController extends Controller
+{
+
+}
