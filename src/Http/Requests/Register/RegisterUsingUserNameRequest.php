f712c8f0e793404dd24210db1b31e5ab1cc9959a
neomn110 <neomn110@gmail.com>
2022-11-23 06:59:56 +0330

    refactor: set user_name field to be required

--- src/Http/Requests/Register/RegisterUsingUserNameRequest.php
+++ src/Http/Requests/Register/RegisterUsingUserNameRequest.php
@@ -25,7 +25,7 @@ class RegisterUsingUserNameRequest extends FormRequest
     public function rules()
     {
         return [
-            'user_name'=>'alpha_dash',
+            'user_name'=>'required|alpha_dash',
             'password'=>'required|min:8|max:32',
             'first_name'=>'required|alpha_spaces|max:32',
             'last_name'=>'required|alpha_spaces|max:32',
