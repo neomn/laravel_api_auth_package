1c84c7e2d8453d973d1370cc46513c95822a0635
Neo <neomn110@gmail.com>
2022-11-24 09:55:29 +0330

    feat: turn raw password to hash

--- src/Http/Controllers/RegisterController.php
+++ src/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace neomn\laravel_auth_api\Http\Controllers;
 
+use Illuminate\Support\Facades\Hash;
 use neomn\laravel_auth_api\Http\Controllers\Controller;
 use neomn\laravel_auth_api\Http\Requests\Register\RegisterUsingUserNameRequest;
 
@@ -10,10 +11,11 @@ class RegisterController extends Controller
     public function registerUsingUserName(RegisterUsingUserNameRequest $request)
     {
         $registrationData = $request->safe()->all();
+        $registrationData['password'] = Hash::make($registrationData['password']);
 
         //register user in userDataBase
 
-        return response()->json('Successfully Registered');
+        return response()->json(['Successfully Registered'=>$registrationData]);
     }
 
     public function registerUsingEmail()
