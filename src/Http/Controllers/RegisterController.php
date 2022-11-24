483491ed16e33bcaf08e72c8c2b244307cf29fa7
Neo <neomn110@gmail.com>
2022-11-24 10:14:14 +0330

    feat: implement some portion of ugistering user using phone number

--- src/Http/Controllers/RegisterController.php
+++ src/Http/Controllers/RegisterController.php
@@ -5,6 +5,7 @@ namespace neomn\laravel_auth_api\Http\Controllers;
 use Illuminate\Support\Facades\Hash;
 use neomn\laravel_auth_api\Http\Controllers\Controller;
 use neomn\laravel_auth_api\Http\Requests\Register\RegisterUsingEmailRequest;
+use neomn\laravel_auth_api\Http\Requests\Register\RegisterUsingPhoneNumberRequest;
 use neomn\laravel_auth_api\Http\Requests\Register\RegisterUsingUserNameRequest;
 
 class RegisterController extends Controller
@@ -29,12 +30,21 @@ class RegisterController extends Controller
 
         //register user in userDataBase
 
+        // notify user using its email that registration is completed
+
         return response()->json(['Successfully Registered'=>$registrationData]);
     }
 
-    public function registerUsingPhoneNumber()
+    public function registerUsingPhoneNumber(RegisterUsingPhoneNumberRequest $request)
     {
+        $registrationData = $request->safe()->all();
+        $registrationData['password'] = Hash::make($registrationData['password']);
 
+        //register user in userDataBase
+
+        // notify user using its phone number that registration is completed
+
+        return response()->json(['Successfully Registered'=>$registrationData]);
     }
 
     public function registerUsingSocialMedia()
