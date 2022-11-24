2dc04449bbe0e3b518e4038c8c6dcb6713cff1a6
Neo <neomn110@gmail.com>
2022-11-24 10:39:31 +0330

    refactor: rename all test_route_exists methods

--- tests/Feature/AuthOperationsTest.php
+++ tests/Feature/AuthOperationsTest.php
@@ -6,43 +6,49 @@ use neomn\laravel_auth_api\tests\TestCase;
 
 class AuthOperationsTest extends TestCase
 {
-    public function test_register_using_username_route_exists()
+    public function test_route_register_using_username_exists()
     {
         $loginResponse = $this->post('api/auth/register/username');
         $this->assertTrue($loginResponse->status() != 404);
     }
 
-    public function test_login_route_exists()
+    public function test_route_register_using_email_exists()
+    {
+        $loginResponse = $this->post('api/auth/register/email');
+        $this->assertTrue($loginResponse->status() != 404);
+    }
+
+    public function test_route_login_exists()
     {
         $loginResponse = $this->post('api/auth/login');
         $this->assertTrue($loginResponse->status() != 404);
     }
 
-    public function test_verify_user_route_exists()
+    public function test_route_verify_user_exists()
     {
         $loginResponse = $this->post('api/auth/verify-user');
         $this->assertTrue($loginResponse->status() != 404);
     }
 
-    public function test_forget_password_route_exists()
+    public function test_route_forget_password_exists()
     {
         $loginResponse = $this->post('api/auth/forget-password');
         $this->assertTrue($loginResponse->status() != 404);
     }
 
-    public function test_update_phone_number_route_exists()
+    public function test_route_update_phone_number_exists()
     {
         $response = $this->json('post' ,'api/auth/update-phone-number');
         $response->assertStatus(401);
     }
 
-    public function test_update_user_info_route_exists()
+    public function test_route_update_user_info_exists()
     {
         $response = $this->json('post' ,'api/auth/update-user-info');
         $response->assertStatus(401);
     }
 
-    public function test_logout_route_exists()
+    public function test_route_logout_exists()
     {
         $response = $this->json('post' ,'api/auth/logout');
         $response->assertStatus(401);
