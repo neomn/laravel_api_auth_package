f31deefc18d6c74b7916473b409dc66f3cdb3251
neomn110 <neomn110@gmail.com>
2022-12-06 22:37:04 +0330

    doc: add hint comment

--- tests/Feature/03_VerifyUserAccountTest.php
+++ tests/Feature/03_VerifyUserAccountTest.php
@@ -4,6 +4,7 @@ namespace neomn\laravel_auth_api\tests\Feature;
 
 use neomn\laravel_auth_api\tests\TestCase;
 
+// verify user account using 2factor-auth methods
 class VerifyUserAccountTest extends TestCase
 {
 
